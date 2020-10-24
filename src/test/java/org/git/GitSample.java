6122f7f3b242290644dd743a25952f06c67b0d4c
sabasiva06 <sabasv06@gmail.com>
2020-10-24 11:28:28 +0530

    siva task

--- src/test/java/org/git/GitSample.java
+++ src/test/java/org/git/GitSample.java
@@ -5,6 +5,9 @@ public class GitSample {
 	
 	public static void main(String[] args) {
 		System.out.println("hii...Helloooo");
+		System.out.println("Day 1");
+		System.out.println("day 2");
+		System.out.println("day3");
 	}
 
 }
